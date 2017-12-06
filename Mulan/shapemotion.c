47eccae7b0750a4ee95baee0fe3cf080b5cb24d7
cjimenez23miner <cjimenez23@miners.utep.edu>
2017-12-05 23:14:35 -0700

    v1.5

--- Mulan/shapemotion.c
+++ Mulan/shapemotion.c
@@ -34,22 +34,21 @@ Layer MyLayer= {
   COLOR_BLACK,
   0,
 };
-////////////////////////////////////////////////////////////////////////
-Layer layer4 = {
-  (AbShape *)&rightArrow,
-  {(screenWidth/2)+10, (screenHeight/2)+5}, /**< bit below & right of center */
-  {0,0}, {0,0},				    /* last & next pos */
-  COLOR_PINK,
+Layer MyLayer2= {
+  (AbShape *)&paddle,
+  {screenWidth-10, screenHeight/2}, /**< center */
+  {0,0}, {0,0},//{15,screenHeight/2}, {screenWidth,screenHeight/2},  /* last & next pos */
+  COLOR_BLACK,
   &MyLayer,
 };
-  
+////////////////////////////////////////////////////////////////////////
 
 Layer layer3 = {		/**< Layer with an orange circle */
   (AbShape *)&circle8,
   {(screenWidth/2)+10, (screenHeight/2)+5}, /**< bit below & right of center */
   {0,0}, {0,0},				    /* last & next pos */
   COLOR_VIOLET,
-  &MyLayer,
+  &MyLayer2,
 };
 
 
@@ -57,7 +56,7 @@ Layer fieldLayer = {		/* playing field as a layer */
   (AbShape *) &fieldOutline,
   {screenWidth/2, screenHeight/2},/**< center */
   {0,0}, {0,0},				    /* last & next pos */
-  COLOR_BLACK,
+  COLOR_BLUE,
   &layer3
 };
 
@@ -89,13 +88,13 @@ typedef struct MovLayer_s {
 
 /* initial value of {0,0} will be overwritten */
 MovLayer ml5 = { &MyLayer, {0,0}, 0}; /// My figure to test
+MovLayer ml6 = { &MyLayer2, {0,0}, 0}; /// My figure to test
 MovLayer ml3 = { &layer3, {1,1}, 0}; /**< not all layers move */
 
-MovLayer ml1 = { &layer1, {1,2}, &ml3 }; 
-MovLayer ml0 = { &layer0, {2,1}, &ml1 }; 
+MovLayer ml1 = { &layer1, {3,2}, &ml3 }; 
+MovLayer ml0 = { &layer0, {-2,3}, &ml1 }; 
 
-
-
+static char points[3];
 
 void movLayerDraw(MovLayer *movLayers, Layer *layers)
 {
@@ -162,31 +161,41 @@ void mlAdvance(MovLayer *ml, Region *fence)
   } /**< for ml */
 }
 
-void mlBounce(MovLayer *ml, MovLayer *fence)
+void mlBounce(MovLayer *ml, MovLayer *fence, MovLayer *fence2)
 {
-  Vec2 newPos;
-  Vec2 padPos;
-  u_char axis;
-  Region shapeBoundary;
-  Region paddleBoundary; // test
-  for (; ml; ml = ml->next) {
-    vec2Add(&newPos, &ml->layer->posNext, &ml->velocity);
-    vec2Add(&padPos, &fence->layer->posNext, &fence->velocity); // fence
-    abShapeGetBounds(ml->layer->abShape, &newPos, &shapeBoundary);
-    abShapeGetBounds(fence->layer->abShape, &padPos, &paddleBoundary); // fence    
-    if (
-        shapeBoundary.topLeft.axes[0] < paddleBoundary.botRight.axes[0] &&
-        (shapeBoundary.botRight.axes[1] > paddleBoundary.topLeft.axes[1] &&
-         shapeBoundary.topLeft.axes[1] < paddleBoundary.botRight.axes[1])
-    ){
-        for (axis = 0; axis < 2; axis ++) {
-            int velocity = ml->velocity.axes[axis] = -ml->velocity.axes[axis];
-            newPos.axes[axis] += (2*velocity);
+    Vec2 newPos;
+    Vec2 padPos, padPos2;
+    u_char axis;
+    Region shapeBoundary;
+    Region paddleBoundary, paddleBoundary2; // test
+    for (; ml; ml = ml->next) {
+        vec2Add(&newPos, &ml->layer->posNext, &ml->velocity);
+        vec2Add(&padPos, &fence->layer->posNext, &fence->velocity); // fence
+        vec2Add(&padPos2, &fence2->layer->posNext, &fence2->velocity);
+        abShapeGetBounds(ml->layer->abShape, &newPos, &shapeBoundary);
+        abShapeGetBounds(fence->layer->abShape, &padPos, &paddleBoundary); // fence    
+        abShapeGetBounds(fence2->layer->abShape, &padPos2, &paddleBoundary2);
+        if ( shapeBoundary.topLeft.axes[0] < paddleBoundary.botRight.axes[0] &&
+            (shapeBoundary.botRight.axes[1] > paddleBoundary.topLeft.axes[1] &&
+             shapeBoundary.topLeft.axes[1] < paddleBoundary.botRight.axes[1])){
+            for (axis = 0; axis < 2; axis ++) {
+                int velocity = ml->velocity.axes[axis] = -ml->velocity.axes[axis];
+                newPos.axes[axis] += (2*velocity);
+            }
+            points[0]= points[0]+1;
+            ml->layer->posNext = newPos;
+        } /**< for ml */
+        else if(shapeBoundary.botRight.axes[0] > paddleBoundary2.topLeft.axes[0] &&
+            (shapeBoundary.botRight.axes[1] > paddleBoundary2.topLeft.axes[1] &&
+            shapeBoundary.topLeft.axes[1] < paddleBoundary2.botRight.axes[1])){
+            for (axis = 0; axis < 2; axis ++) {
+                int velocity = ml->velocity.axes[axis] = -ml->velocity.axes[axis];
+                newPos.axes[axis] += (2*velocity);
+            }
+            points[2]= points[2]+1;
+            ml->layer->posNext = newPos;
         }
     }
-    
-    ml->layer->posNext = newPos;
-  } /**< for ml */
 }
 
 u_int bgColor = COLOR_BLUE;     /**< The background color */
@@ -198,71 +207,96 @@ Region fieldFence;		/**< fence around playing field  */
 /** Initializes everything, enables interrupts and green LED, 
  *  and handles the rendering for the screen
  */
-void main()
+void game()
 {
-  P1DIR |= GREEN_LED;		/**< Green led on when CPU on */		
-  P1OUT |= GREEN_LED;
-
-  configureClocks();
-  lcd_init();
-  shapeInit();
-  p2sw_init(15);
-
-  shapeInit();
-
-  layerInit(&layer0);
-  layerDraw(&layer0);
-
-
-  layerGetBounds(&fieldLayer, &fieldFence);
-
-
-  enableWDTInterrupts();      /**< enable periodic interrupt */
-  or_sr(0x8);	              /**< GIE (enable interrupts) */
+  points[0]='0';
+  points[2]='0';
+  points[1]='|';
+  int vic=0;
 
 /////////////////////////////////////
     Vec2 padPos;
     vec2Add(&padPos, &ml5.layer->posNext, &ml5.velocity);
-///////////////////////////////////
-    
+    Vec2 padPos2;
+    vec2Add(&padPos2, &ml6.layer->posNext, &ml6.velocity);
+    ///////////////////////////////////
   for(;;) {
-      
-//////////////////Reseting buttons
-      char str[5];
-///////////////////////////////
-    while (!redrawScreen) { /**< Pause CPU if screen doesn't need updating */
-      P1OUT &= ~GREEN_LED;    /**< Green led off witHo CPU */
-      or_sr(0x10);	      /**< CPU OFF */
-    }
-    P1OUT |= GREEN_LED;       /**< Green led on when CPU on */
-    redrawScreen = 0;
-    movLayerDraw(&ml0, &layer0);
-    movLayerDraw(&ml5, &MyLayer);
+    if(points[0]<'5'&& points[2]<'5'){
+        vic=0;
+        drawString5x7(screenWidth/2-5,2, points, COLOR_GREEN, COLOR_BLUE);
+    //////////////////Reseting buttons
+        char str[5];
 
-/////////////////////////////////////////////////////////////////////////////////////
-    u_char width = screenWidth, height = screenHeight;
+        while (!redrawScreen) { /**< Pause CPU if screen doesn't need updating */
+        P1OUT &= ~GREEN_LED;    /**< Green led off witHo CPU */
+        or_sr(0x10);	      /**< CPU OFF */
+        }
+        P1OUT |= GREEN_LED;       /**< Green led on when CPU on */
+        redrawScreen = 0;
+        movLayerDraw(&ml0, &layer0);
+        movLayerDraw(&ml5, &MyLayer);
+        movLayerDraw(&ml6, &MyLayer2);
 
-  //  drawString5x7(10,10, "switches:", COLOR_GREEN, COLOR_BLUE);
-    u_int switches = p2sw_read(), i;
-    
-    for (i = 0; i < 4; i++){
-        str[i] = (switches & (1<<i)) ? '-' : '1'+i;
+        u_char width = screenWidth, height = screenHeight;
+
+        u_int switches = p2sw_read(), i;
+        
+        for (i = 0; i < 4; i++){
+            str[i] = (switches & (1<<i)) ? '-' : '1'+i;
+        }  
+        if(str[0]=='1' && padPos.axes[1]>10){
+            int velocity = ml5.velocity.axes[1] = -3;
+            padPos.axes[1] += (2*velocity);
+        } else if(str[1]=='2' && padPos.axes[1]<(height-10)){
+            int velocity = ml5.velocity.axes[1] = 3;
+            padPos.axes[1] += (2*velocity);
+        }
+        
+        if(str[2]=='3' && padPos2.axes[1]>10){
+            int velocity = ml5.velocity.axes[1] = -3;
+            padPos2.axes[1] += (2*velocity);
+        } else if(str[3]=='4' && padPos2.axes[1]<(height-10)){
+            int velocity = ml5.velocity.axes[1] = 3;
+            padPos2.axes[1] += (2*velocity);
+        }
+
+        str[4] = 0;
+        
+        ml5.layer->posNext = padPos;
+        ml6.layer->posNext = padPos2;
+    }else{
+        clearScreen(COLOR_BLUE);
+        drawString5x7(screenWidth/2,screenHeight/2, "YOU WON", COLOR_BLACK, COLOR_BLUE);
+        vic++;
+        if(vic==10){
+            points[0]='0';
+            points[2]='0';
+            ml5.layer->posNext.axes[0]=screenWidth/2;
+            ml5.layer->posNext.axes[1]=screenHeight/2;
+            ml6.layer->posNext.axes[0]=screenWidth/2;
+            ml6.layer->posNext.axes[1]=screenHeight/2;
+        }
     }
-//////////////Sub experiment    
-    if(str[0]=='1' && padPos.axes[1]>10){
-        int velocity = ml5.velocity.axes[1] = -3;
-        padPos.axes[1] += (2*velocity);
-    } else if(str[1]=='2' && padPos.axes[1]<(height-10)){
-        int velocity = ml5.velocity.axes[1] = 3;
-        padPos.axes[1] += (2*velocity);
-    }
-/////////////   
-    str[4] = 0;
-  //  drawString5x7(20,20, str, COLOR_GREEN, COLOR_BLUE);
-///////////////////////////////////////////////////////////
-      ml5.layer->posNext = padPos;
-    
   }
+}
+
+void main(){
+    P1DIR |= GREEN_LED;		/**< Green led on when CPU on */		
+    P1OUT |= GREEN_LED;
+
+    configureClocks();
+    lcd_init();
+    shapeInit();
+    p2sw_init(15);
+
+    shapeInit();
+
+    layerInit(&layer0);
+    layerDraw(&layer0);
+    layerGetBounds(&fieldLayer, &fieldFence);
+    enableWDTInterrupts();      /**< enable periodic interrupt */
+    or_sr(0x8);	              /**< GIE (enable interrupts) */
+    game();
 }
 
 /** Watchdog timer interrupt handler. 15 interrupts/sec */
@@ -271,10 +305,10 @@ void wdt_c_handler()
   static short count = 0;
   P1OUT |= GREEN_LED;		      /**< Green LED on when cpu on */
   count ++;
-  if (count == 15) {
+  if (count == 20) {
     mlAdvance(&ml0, &fieldFence);
-    mlBounce(&ml0, &ml5); //test
- //   if (p2sw_read())
+    mlBounce(&ml0, &ml5, &ml6); //test
+    if (~p2sw_read())
       redrawScreen = 1;
     count = 0;
   }
